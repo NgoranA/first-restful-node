2551fae4428a0d67cc3258b0baca2c8147d91418
NgoranA <ngoranaristide4@gmail.com>
2025-04-15 16:36:55 +0100

    configured error handling for the project

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
+import createError from "http-errors"
 
-import indexRouter from './routes/index.js'
-import usersRouter from './routes/users.js'
+import indexRouter from './routes/bicycle.js'
 
 const app = express();
 
@@ -12,7 +12,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/bicycle', indexRouter);
+
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 export default app
